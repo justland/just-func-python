da29737018de6c1d51b32dd2f98362edd5e5c5f1
Vincent White <xstrengthofonex@gmail.com>
2021-03-10 02:12:36 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="justfunc",
+    description="A python interpreter for the just-func language",
+    version="0.1dev",
+    url="https://github.com/justland/just-func-python",
+    packages=["justfunc"],
+    license="MIT",
+)
