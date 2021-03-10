91e069027ba9427c6eb56ff52561d91c26c62577
Vincent White <xstrengthofonex@gmail.com>
2021-03-11 00:57:59 +0900

    Implement subtract operator

--- justfunc/interpreter.py
+++ justfunc/interpreter.py
@@ -1,3 +1,4 @@
+from functools import reduce
 
 
 def evaluate(source, env=None):
@@ -6,11 +7,20 @@ def evaluate(source, env=None):
             return 0
         return sum(_evaluate(arg, ctx) for arg in args)
 
+    def _subtract(args, ctx):
+        if len(args) == 1:
+            return -_evaluate(args[0], ctx)
+        return reduce(
+            lambda x, y: x - y,
+            (_evaluate(arg, ctx) for arg in args))
+
     def _evaluate(expr, ctx):
         if type(expr) == tuple:
             symbol, *args = expr
             if symbol == "+":
                 return _add(args, ctx)
+            if symbol == "-":
+                return _subtract(args, ctx)
             value = ctx.get(symbol)
             if value is not None:
                 return _evaluate(value, ctx)
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -11,3 +11,11 @@ def test_add():
     assert evaluate(("+", ("x",), ("y",)), dict(x=1, y=2)) == 3
     assert evaluate(("+", 1, ("y",)), dict(y=("+", 2, 3))) == 6
 
+
+def test_subtract():
+    assert evaluate(("-", 1)) == -1
+    assert evaluate(("-", 1000, 334)) == 666
+    assert evaluate(("-", 10.6, 2.3)) == 8.3
+    assert evaluate(("-", ("-", 3))) == 3
+    assert evaluate(("-", ("-", 10, 2), ("-", 3, 1))) == 6
+    assert evaluate(("-", ('x',), ("y",)), dict(y=5, x=10)) == 5
