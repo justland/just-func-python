577f21589b7c345ca4a160ba2959fe6e6d518b52
Vincent White <xstrengthofonex@gmail.com>
2021-03-18 00:24:47 +0900

    Update setup and add LICENSE

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 
 setup(
-    name="justfunc",
-    description="A python interpreter for the just-func language",
+    name="just-func",
     version="0.1dev",
+    author="Vincent White",
+    author_email="xstrengthofonex@gmail.com",
+    description="A python interpreter for the just-func language",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/justland/just-func-python",
-    packages=["justfunc"],
-    license="MIT",
+    package_dir={"": "justfunc"},
+    packages=find_packages(where="justfunc")
 )
